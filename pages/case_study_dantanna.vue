954686d82281a5ed9099f55d35739402a056f79a
Jabatronic <jabatronic@gmail.com>
2019-09-08 19:23:47 +0100

    create case study page

---
+++ pages/case_study_dantanna.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container content">
+    <div class="columns">
+      <div class="column is-7">
+        <h1 class="title is-2 jtron-logotype">
+          Case Study: Dantanna Beatz
+        </h1>
+        <div>
+          <p>
+            I am a web developer/designer with ability, knowledge, and experience across the
+            <a href="#">full-stack</a> but weighted towards the
+            <a href="#">front-end</a>.
+          </p>
+          <p>
+            <a href="#">HTML5</a>,
+            <a href="#">CSS3</a>, and
+            <a href="#">JavaScript</a> are part of my everyday vocabulary and custom
+            <a href="#">Wordpress</a> sites make up a large part of my <a href="#">commercial
+              experience</a>.
+          </p>
+          <p />
+        </div>
+      </div>
+      <div class="column is-5">
+        <!-- MAIN CONTENT WILL LIVE HERE -->
+
+        <div class="columns">
+          <div class="column">
+            <h2 class="has-text-grey-lighter ">
+              Recent Works
+            </h2>
+            <hr>
+            <p class="menu-label">
+              Dantanna Beatz
+            </p>
+            <div class="image folio-tile box">
+              <img src="~/assets/images/dantanna_frontpage_folio.jpg" alt>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p class="menu-label">
+              First Wealth
+            </p>
+            <div class="image folio-tile box">
+              <img src="~/assets/images/first_wealth_frontpage_folio.jpg" alt>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p class="menu-label">
+              The Ice Chef
+            </p>
+            <div class="image folio-tile box">
+              <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p class="menu-label">
+              EDAS
+            </p>
+            <div class="image folio-tile box">
+              <img src="~/assets/images/edas_frontpage_folio.jpg" alt>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p class="menu-label">
+              The Little Big Programme
+            </p>
+            <div class="image folio-tile box">
+              <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
+            </div>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p class="menu-label">
+              The Big Programme
+            </p>
+            <div class="image folio-tile box">
+              <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
+            </div>
+          </div>
+        </div>
+        <!-- *************************** -->
+      </div>
+    </div>
+  </div>
+</template>
