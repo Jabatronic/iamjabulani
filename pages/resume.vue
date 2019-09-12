beab5e217c5e3131bcb6de394b34bf8b4dd3ad99
Jabatronic <jabatronic@gmail.com>
2019-09-12 16:18:32 +0100

    create resume

---
+++ pages/resume.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container content">
+    <div class="columns is-fullheight is-centered">
+      <!-- Personal Deets -->
+      <div class="column is-3 is-teal has-text-white  ">
+        <figure class="image is-128x128 is-marginless">
+          <img class="is-rounded" src="~/assets/images/jabulani_robbins.jpg" alt="">
+        </figure>
+        <div class="margin-top-40 ">
+          <br>
+          <br>
+          <h3 class="has-text-white has-font-roboto-slab">
+            Contact
+          </h3>
+          <address>
+            93 Parkstone Road<br>
+            Poole, Dorset<br>
+            BH15 2NZ
+          </address>
+          <br>
+          <p>
+            T: (+44) 7969 910 258
+          </p>
+          <p>E: jabu.robbins@gmail.com</p>
+          <h3 class="has-text-white has-font-roboto-slab ">
+            Interests
+          </h3>
+          <ul>
+            <li>Electric Guitar</li>
+            <li>Art &amp; Design</li>
+            <li> Science &amp; Tech</li>
+            <li>Current Affairs</li>
+            <li>Yoga &amp; Meditation</li>
+          </ul>
+        </div>
+      </div>
+      <div class="column is-9">
+        <h1 class="title is-uppercase has-font-roboto-slab">
+          M Jabulani Robbins
+        </h1>
+        <hr class="is-teal">
+        <h3>Career Objective</h3>
+        <p>To gain experience with forward thinking digital agencies and develop my use of modern development techniques.</p>
+
+        <div class="columns">
+          <div class="column is-6">
+            <h4>Education</h4>
+            <ul>
+              <li><h5>MA Media Arts (Distinction)</h5></li>
+              <h7>Research Topics Included:</h7>
+              <ul>
+                <li>Visual Language and Semiotics</li>
+                <li>Human Computer Interaction</li>
+                <li>Interaction Design</li>
+              </ul>
+              <li><h5>BA(Hons) Upper Second Class - Interactive Media Communication</h5></li>
+              <h7>Modules Included:</h7>
+              <ul>
+                <li>HTML, JavaScript, CSS</li>
+                <li>User Centered Design</li>
+                <li>Multimedia Design - [Kiosk and embedded installations]</li>
+              </ul>
+            </ul>
+          </div>
+          <div class="column is-6">
+            <h4>Technical Proficiencies</h4>
+            <p>
+              HTML5, CSS3, JavaScript, jQuery, VueJs, PHP, Content Management Systems (Wordpress), PHP MVC Frameworks (Laravel),
+              Modern build tools (Gulp, webpack, sass)
+              Exemplary written and verbal communication skills
+            </p>
+            <h4>Other Certificates</h4>
+            <ul>
+              <li>Level 3 Diploma in Applied Therapeutic Skills</li>
+              <li>Theater for conflict resolution</li>
+              <li>Education for Sustainable development</li>
+            </ul>
+          </div>
+        </div>
+        <h2 class="title">
+          Experience
+        </h2>
+        <hr
+          class="is-teal"
+        >
+
+        <ul>
+          <li>
+            <h4>Freelance</h4>
+            <ul>
+              <li>2017 - Present: Freelance Web Developer</li>
+            </ul>
+          </li>
+          <h4>Wolverhampton University School of Art &amp; Design</h4>
+          <li>
+            <p>
+              20??: Course Leader in Digital Arts &amp; Media (University of Wolverhampton)
+            </p>
+          </li>
+          <li>
+            <p>
+              20??: Subject Leader in Computer Games Design  (University of Wolverhampton)
+            </p>
+          </li>
+        </ul>
+        <p>
+          20??: Graduate Placement (Arkwright Consulting)
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+/* For da debug */
+.border-red {
+    border: 1px solid red;
+}
+</style>
