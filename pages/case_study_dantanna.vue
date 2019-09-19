081b72af34f6689a158b0a686cf9e96b7c9473e5
Jabatronic <jabatronic@gmail.com>
2019-09-19 13:12:56 +0100

    add placeholder and guide text for sections

--- pages/case_study_dantanna.vue
+++ pages/case_study_dantanna.vue
@@ -1,16 +1,49 @@
 <template>
   <div class="container content">
     <div class="columns">
-      <div class="column is-7">
+      <div class="column is-9">
         <h1 class="title is-2 jtron-logotype">
           Case Study: Dantanna Beatz
         </h1>
         <div>
+          <!-- Start Introduction -->
+          <h3>Overview</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur reprehenderit, facere doloribus rem, commodi aperiam eius consequuntur dolorem mollitia repellendus quod sequi ab quam vitae quo soluta qui labore possimus!</p>
+
+          <h3>The Brief</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ullam quis ad quasi maxime provident. Doloribus odit corporis, repellat enim quaerat a iure, labore non sint doloremque deserunt deleniti delectus!</p>
+
+          <h3>Users &amp; Audience</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A quisquam laborum doloremque magni ullam, itaque earum repudiandae voluptate sint incidunt ab. Repellat corrupti odit ea excepturi optio a deleniti consequuntur.</p>
+
+          <h3>Roles &amp; Responsibilities</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam excepturi, culpa, impedit voluptatum corporis tenetur eius quia commodi esse reiciendis reprehenderit nobis eaque doloribus, quo est rerum eligendi unde dolorem.</p>
+
+          <h3>Scope &amp; Constraints</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A molestiae ratione illum! Doloremque repellat consectetur aliquam porro in nam beatae cum repudiandae labore. Totam recusandae illum odio expedita ipsam nihil!</p>
+          <!-- End Introduction -->
+
+          <!-- Start Process -->
+          <h3>Process / What I did</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eaque, eius explicabo earum blanditiis delectus ipsa sunt numquam culpa ex, at accusantium quibusdam, ducimus saepe? Optio nesciunt nulla fuga non!</p>
+          <!-- End Process -->
+          <h3>Conclusion</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eaque, eius explicabo earum blanditiis delectus ipsa sunt numquam culpa ex, at accusantium quibusdam, ducimus saepe? Optio nesciunt nulla fuga non!</p>
+          <!-- Start Conclusion -->
+
+          <!-- End Conclusion -->
         </div>
       </div>
-      <div class="column is-5">
+      <div class="column is-3">
         <!-- MAIN CONTENT WILL LIVE HERE -->
+
+        <!-- Start Skills Used -->
+
+        <!-- End Skills Used -->
+
+        <!-- Start Thumbnail Gallery -->
+
+        <!-- End Thumbnail Gallery -->
 
         <div class="columns">
           <div class="column">
