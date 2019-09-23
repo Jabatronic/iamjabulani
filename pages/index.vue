37ed786d4c4edfa12bf6263cd4b1395de3477b9a
Jabatronic <jabatronic@gmail.com>
2019-09-23 21:13:24 +0100

    remove sidebar content and insert component

--- pages/index.vue
+++ pages/index.vue
@@ -27,76 +27,21 @@
         </div>
       </div>
       <div class="column is-5">
-        <!-- MAIN CONTENT WILL LIVE HERE -->
-
-        <div class="columns">
-          <div class="column">
-            <h2 class="has-text-grey-lighter ">
-              Recent Works
-            </h2>
-            <hr>
-            <p class="menu-label">
-              Dantanna Beatz
-            </p>
-            <div class="image folio-tile box">
-              <nuxt-link to="case_study_dantanna">
-                <img src="~/assets/images/dantanna_frontpage_folio.jpg" alt>
-              </nuxt-link>
-            </div>
-          </div>
-        </div>
-        <div class="columns">
-          <div class="column">
-            <p class="menu-label">
-              First Wealth
-            </p>
-            <div class="image folio-tile box">
-              <img src="~/assets/images/first_wealth_frontpage_folio.jpg" alt>
-            </div>
-          </div>
-        </div>
-        <div class="columns">
-          <div class="column">
-            <p class="menu-label">
-              The Ice Chef
-            </p>
-            <div class="image folio-tile box">
-              <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
-            </div>
-          </div>
-        </div>
-        <div class="columns">
-          <div class="column">
-            <p class="menu-label">
-              EDAS
-            </p>
-            <div class="image folio-tile box">
-              <img src="~/assets/images/edas_frontpage_folio.jpg" alt>
-            </div>
-          </div>
-        </div>
-        <div class="columns">
-          <div class="column">
-            <p class="menu-label">
-              The Little Big Programme
-            </p>
-            <div class="image folio-tile box">
-              <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
-            </div>
-          </div>
-        </div>
-        <div class="columns">
-          <div class="column">
-            <p class="menu-label">
-              The Big Programme
-            </p>
-            <div class="image folio-tile box">
-              <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
-            </div>
-          </div>
-        </div>
-        <!-- *************************** -->
+        <!-- Start Sidebar Widgets-->
+        <recent-works />
+        <!-- End Sidebar Widgets -->
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import RecentWorks from '~/components/sidebar/RecentWorks.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    RecentWorks
+  }
+}
+</script>
