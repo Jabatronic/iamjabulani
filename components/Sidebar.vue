3b3f04f34924ba7d183ac07b0a0376a9c4940402
Jabatronic <jabatronic@gmail.com>
2019-09-23 20:24:12 +0100

    extract sidebar to component

---
+++ components/Sidebar.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <h2 class="has-text-grey-lighter">
+          Recent Works
+        </h2>
+        <hr>
+        <p class="menu-label">
+          Dantanna Beatz
+        </p>
+        <div class="image folio-tile box">
+          <img src="~/assets/images/dantanna_frontpage_folio.jpg" alt>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <p class="menu-label">
+          First Wealth
+        </p>
+        <div class="image folio-tile box">
+          <img src="~/assets/images/first_wealth_frontpage_folio.jpg" alt>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <p class="menu-label">
+          The Ice Chef
+        </p>
+        <div class="image folio-tile box">
+          <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <p class="menu-label">
+          EDAS
+        </p>
+        <div class="image folio-tile box">
+          <img src="~/assets/images/edas_frontpage_folio.jpg" alt>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <p class="menu-label">
+          The Little Big Programme
+        </p>
+        <div class="image folio-tile box">
+          <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <p class="menu-label">
+          The Big Programme
+        </p>
+        <div class="image folio-tile box">
+          <img src="~/assets/images/the_ice_chef_frontpage_folio.jpg" alt>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
