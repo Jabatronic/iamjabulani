575c970f40970a0059a00157e600fc11b7871b99
Jabatronic <jabatronic@gmail.com>
2019-10-07 16:16:44 +0100

    add image handling component

---
+++ components/JImage.vue
@@ -0,0 +1,16 @@
+<template>
+  <img :srcset="imgUrl.srcSet" alt="">
+</template>
+
+<script>
+const defImage = require('~/assets/images/placeholder.png')
+export default {
+  name: 'JImage',
+  props: {
+    imgUrl: {
+      type: String,
+      default: defImage
+    }
+  }
+}
+</script>
