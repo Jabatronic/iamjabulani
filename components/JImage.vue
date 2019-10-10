2721d531436556aa81d28f53e0028d3f8ede16d5
Jabatronic <jabatronic@gmail.com>
2019-10-10 13:42:35 +0100

    usage instructions

--- components/JImage.vue
+++ components/JImage.vue
@@ -4,12 +4,22 @@
     <source :srcset="imgUrl.fallback">
     <img :src="imgUrl.source" :alt="imgUrl.alt">
   </picture>
+  <!-- Example Usage
+<j-image
+  :img-url=" {
+    webp: require('~/assets/images/dantanna_full-page_full-size.png?webp'),
+    fallback: require('~/assets/images/dantanna_full-page_full-size.png'),
+    source: require('~/assets/images/dantanna_full-page_full-size.png'),
+    alt: 'The DantannaBeatz Homepage',
+    class: 'j-image'
+  }"
+/> -->
 </template>
 
 <script>
 const defImage = {
   webp: require('~/assets/images/placeholder.png?webp'),
-  fallback: require('~/assets/images/placeholder.png'),
+  fallback: require('~/assets/images/placeholder.png').srcSet,
   source: require('~/assets/images/placeholder.png'),
   alt: 'Image not available'
 }
