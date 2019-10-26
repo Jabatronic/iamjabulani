01ba1069a621a088bb855f6c7d4d3c7b59dd6841
Jabatronic <jabatronic@gmail.com>
2019-10-26 13:21:29 +0100

    To Do: Add Roboto locally

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,8 +12,8 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      // { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans|Roboto|Roboto+Slab&display=swap' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap' }
 
     ],
     bodyAttrs: {
