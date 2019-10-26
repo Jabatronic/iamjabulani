527fd851671e3f2a5255e82ea9a30ca6cba66c8e
Jabatronic <jabatronic@gmail.com>
2019-10-26 11:02:01 +0100

    comment change

--- modules/typefaces.js
+++ modules/typefaces.js
@@ -1,5 +1,5 @@
 export default function typeface (options) {
-  // Add CSS
+  // Add font CSS
   this.options.css.push('typeface-roboto-slab/index.css')
   this.options.css.push('typeface-raleway/index.css')
 }
