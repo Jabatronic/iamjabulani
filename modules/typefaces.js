d98c2c0619fec3d6a025bcc238061622e8149b56
Jabatronic <jabatronic@gmail.com>
2019-10-26 14:19:26 +0100

    add Roboto to font stack!

--- modules/typefaces.js
+++ modules/typefaces.js
@@ -1,5 +1,6 @@
 export default function typeface (options) {
   // Add font CSS
   this.options.css.push('typeface-roboto-slab/index.css')
+  this.options.css.push('typeface-roboto/index.css')
   this.options.css.push('typeface-raleway/index.css')
 }
