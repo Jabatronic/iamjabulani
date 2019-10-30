9f1065386fb39cf0c1754cb569fa26cce84747dc
Jabatronic <jabatronic@gmail.com>
2019-10-30 07:46:46 +0000

    add intro animations

--- pages/index.vue
+++ pages/index.vue
@@ -4,22 +4,35 @@
       <div class="container">
         <div class="columns is-centered">
           <div class="column is-2">
-            <figure class="image container is-128x128">
+            <figure
+              data-aos="zoom-in"
+              data-aos-duration="400"
+              class="image container is-128x128"
+            >
               <img
-                class="is-rounded"
+                class="is-rounded is-me"
                 :src="require('~/assets/images/jabulani_robbins.jpg').src"
                 :srcSet="require('~/assets/images/jabulani_robbins.jpg').srcSet"
               >
             </figure>
           </div>
           <div class="column is-5">
-            <h1 class="title is-size-2">
-              <span class="has-text-teal has-font-roboto-slab">Jabulani Robbins</span>
-            </h1>
-            <h2 class="subtitle">
-              <b>Web Development Portfolio &amp; Resume</b>
-            </h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus dicta voluptatibus, omnis fuga velit itaque, nihil nemo laboriosam, odit voluptas fugiat dignissimos commodi hic quod? Ullam alias rerum error. Ducimus?</p>
+            <div
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
+              <h1 class="title is-size-2">
+                <span class="has-text-teal has-font-roboto-slab">I am Jabulani Robbins</span>
+              </h1>
+              <h2 class="subtitle">
+                <b>Web Developer</b>
+              </h2>
+              <p>
+                I've been working with web technologies since ????
+                I love all things Science &amp; Tech. All things, Art &amp; Design.
+                Check out some of the projects I've worked on.
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -44,5 +57,9 @@ export default {
   // }
   p.intro-text {
     width: 100%;
+  }
+
+  img.is-me {
+    border: 1px solid $grey-lighter;
   }
 </style>
