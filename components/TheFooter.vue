6aa5cd913de6ca69eb3a6f776693fc51d817e5c4
Jabatronic <jabatronic@gmail.com>
2019-10-31 20:54:35 +0000

    rename and refactor footer component

---
+++ components/TheFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="sticky-footer">
+    <div class="hero-foot">
+      <div class="container">
+        <div class="tabs is-centered">
+          <ul>
+            <li>
+              <a>Contact Me!</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+  // Footer mods
+  footer.sticky-footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    width: 100vw;
+    background: #fff;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,20 +4,18 @@
     <main class="jtron-main">
       <nuxt />
     </main>
-    <footer class="sticky-footer">
-      <!-- <app-footer /> -->
-    </footer>
+    <the-footer />
   </div>
 </template>
 
 <script>
 import TopNav from '~/components/TopNav.vue'
-// import AppFooter from '~/components/AppFooter.vue'
+import TheFooter from '~/components/TheFooter.vue'
 
 export default {
   components: {
-    TopNav
-    // AppFooter
+    TopNav,
+    TheFooter
   }
 }
 </script>
@@ -28,15 +26,4 @@ p {
     max-width: 40rem;
     font-size: 1.2rem;
 }
-
-  // // Footer mods
-  // footer.sticky-footer {
-  //   position: fixed;
-  //   bottom: 0;
-  //   left: 0;
-  //   right: 0;
-  //   z-index: 10;
-  //   width: 100vw;
-  //   background: #fff;
-  // }
 </style>
