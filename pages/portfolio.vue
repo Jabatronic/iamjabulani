c4b8d0f24ac895c893b8e6c9a89e9746810076ff
Jabatronic <jabatronic@gmail.com>
2019-11-01 09:28:00 +0000

    extract portfolio to component

---
+++ pages/portfolio.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <the-portfolio />
+  </div>
+</template>
+
+<script>
+import ThePortfolio from '~/components/ThePortfolio.vue'
+export default {
+  components: {
+    ThePortfolio
+  }
+}
+</script>
