d8d4a7cf127102b9713b25ca9ce6445c7da4bba8
Jabatronic <jabatronic@gmail.com>
2019-11-13 14:17:28 +0000

    experimenting with  tiled layout

--- components/ThePortfolio.vue
+++ components/ThePortfolio.vue
@@ -5,13 +5,13 @@
         <div class="tile is-parent is-vertical">
           <article class="tile is-child notification is-primary">
             <p class="title">
-              Vertical...
+              Vertical...xxxx
             </p>
             <p class="subtitle">
               Top tile
             </p>
           </article>
-          <article class="tile is-child notification is-warning">
+          <article class="tile is-child notification is-primary">
             <p class="title">
               ...tiles
             </p>
@@ -21,7 +21,7 @@
           </article>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child notification is-info">
+          <article class="tile is-child notification is-primary" style="background: url(https://bulma.io/images/placeholders/640x480.png) center/100%">
             <p class="title">
               Middle tile
             </p>
@@ -29,7 +29,7 @@
               With an image
             </p>
             <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/640x480.png">
+              <!-- <img src="https://bulma.io/images/placeholders/640x480.png"> -->
             </figure>
           </article>
         </div>
@@ -41,9 +41,10 @@
           </p>
           <p class="subtitle">
             Aligned with the right tile
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum pariatur ullam, mollitia delectus suscipit debitis quis accusamus dignissimos! Deserunt pariatur praesentium totam. Rerum rem quis repellat mollitia molestiae consequatur accusamus?
           </p>
           <div class="content">
-            <!-- Content -->
+          <!-- Content -->
           </div>
         </article>
       </div>
@@ -58,10 +59,59 @@
             With even more content
           </p>
           <div class="content">
-            <!-- Content -->
+          <!-- Content -->
           </div>
         </div>
       </article>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// for da debug
+
+.tile {
+  // height: 30rem;
+}
+
+  /* Folio Tiles */
+article {
+  // padding: 0;
+  overflow: hidden;
+}
+
+article img {
+  transition: all 0.6s;
+}
+
+article:hover img {
+    transform: scale(1.2);
+    cursor: pointer;
+}
+
+// article p {
+  // visibility: hidden;
+// }
+
+// .tile.is-gapless {
+//   .tile.is-parent {
+//     padding: 0;
+//   }
+
+//   .tile.is-vertical>.tile.is-child {
+//     margin-bottom: 0 !important;
+//   }
+
+//   .notification {
+//     border-radius: 0;
+//   }
+// }
+</style>
