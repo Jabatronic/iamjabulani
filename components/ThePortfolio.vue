4a5cdfe5193ff41dcb1d0f0b3ce60b06c47b997b
Jabatronic <jabatronic@gmail.com>
2019-11-14 13:22:09 +0000

    restyle Article tile section

--- components/ThePortfolio.vue
+++ components/ThePortfolio.vue
@@ -21,21 +21,26 @@
           </article>
         </div>
         <div class="tile is-parent">
-          <article class="tile is-child notification is-primary is-folio-thumbnail" style="background-image: url(https://bulma.io/images/placeholders/640x480.png)">
-            <p class="title">
-              Middle tile
-            </p>
-            <p class="subtitle">
-              With an image
-            </p>
-            <figure class="image is-4by3">
-              <!-- <img src="https://bulma.io/images/placeholders/640x480.png"> -->
-            </figure>
+          <article class="tile is-child notification is-primary is-folio-thumbnail">
+            <div
+              class="bg-image is-overlay"
+              :style="{ backgroundImage: `url(${img1.src})`}"
+            />
+            <div class="is-overlay folio-thumbnail-details">
+              <p class="title">
+                Middle tile
+              </p>
+              <p class="subtitle">
+                With an image
+              </p>
+            </div>
           </article>
         </div>
       </div>
       <div class="tile is-parent">
-        <article class="tile is-child notification is-primary is-folio-thumbnail">
+        <article
+          class="tile is-child notification is-primary is-folio-thumbnail"
+        >
           <p class="title">
             Wide tile
           </p>
@@ -67,9 +72,12 @@
   </div>
 </template>
 <script>
+const firstWealthImg = require('~/assets/images/case_study/first_wealth/the_team.jpg')
+
 export default {
   data () {
     return {
+      img1: firstWealthImg
     }
   }
 }
@@ -78,12 +86,43 @@ export default {
 <style lang="scss" scoped>
 /* Folio Tiles */
 .tile .is-folio-thumbnail {
-  background-size: 100%;
-  background-position: center;
-  transition: all 0.6s;
+  overflow: hidden;
+
+  & .bg-image {
+    opacity: 1;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    transition: all 0.6s;
+    padding: 20px;
+  }
+  &:hover .bg-image {
+    opacity: .4;
+  }
+
+  & .folio-thumbnail-details {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    border-radius: $radius;
+    margin: 0 important;
+    opacity: 0;
+    transition: all .6s;
+
+    & .title {
+      font-family: $roboto-slab;
+      text-align: center;
+    }
+  }
 
   &:hover {
-    background-size: 120%;
+    .folio-thumbnail-details {
+      opacity: 1;
+    }
   }
 }
 
