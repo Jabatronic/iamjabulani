d2f9693e0eb8c61d92bac566786f1e5a9a74c7ac
Jabatronic <jabatronic@gmail.com>
2019-11-14 13:31:53 +0000

    create  Article child comp

---
+++ components/portfolio/PortfolioArticle.vue
@@ -0,0 +1,16 @@
+<template>
+    <article class="tile is-child notification is-primary is-folio-thumbnail">
+    <div
+        class="bg-image is-overlay"
+        :style="{ backgroundImage: `url(${img1.src})`}"
+    />
+    <div class="is-overlay folio-thumbnail-details">
+        <p class="title">
+        Middle tile
+        </p>
+        <p class="subtitle">
+        With an image
+        </p>
+    </div>
+    </article>  
+</template>
