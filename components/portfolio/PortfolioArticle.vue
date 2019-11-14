a28c9c13e41fd3e553299b8a3ed6d77cc79096c4
Jabatronic <jabatronic@gmail.com>
2019-11-14 14:04:34 +0000

    extract css to child comp

--- components/portfolio/PortfolioArticle.vue
+++ components/portfolio/PortfolioArticle.vue
@@ -1,16 +1,74 @@
 <template>
-    <article class="tile is-child notification is-primary is-folio-thumbnail">
+  <article
+    class="tile is-child notification is-primary is-folio-thumbnail"
+    :style="{ height: `${thumbImage.height}.px` }"
+  >
     <div
-        class="bg-image is-overlay"
-        :style="{ backgroundImage: `url(${img1.src})`}"
+      class="bg-image is-overlay"
+      :style="{ backgroundImage: `url(${thumbImage.src})`}"
     />
     <div class="is-overlay folio-thumbnail-details">
-        <p class="title">
+      <p class="title">
         Middle tile
-        </p>
-        <p class="subtitle">
+      </p>
+      <p class="subtitle">
         With an image
-        </p>
+      </p>
     </div>
-    </article>  
+  </article>
 </template>
+<script>
+export default {
+  name: 'PortfolioArticle',
+  props: {
+    thumbImage: {
+      type: String,
+      default: '~/assets/images/placeholder.png'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+/* Folio Tiles */
+.tile .is-folio-thumbnail {
+  overflow: hidden;
+
+  & .bg-image {
+    opacity: 1;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    transition: all 0.6s;
+    padding: 20px;
+  }
+  &:hover .bg-image {
+    opacity: .4;
+  }
+
+  & .folio-thumbnail-details {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    border-radius: $radius;
+    margin: 0 important;
+    opacity: 0;
+    transition: all .6s;
+
+    & .title {
+      font-family: $roboto-slab;
+      text-align: center;
+    }
+  }
+
+  &:hover {
+    .folio-thumbnail-details {
+      opacity: 1;
+    }
+  }
+}
+</style>
--- components/portfolio/ThePortfolio.vue
+++ components/portfolio/ThePortfolio.vue
@@ -38,20 +38,7 @@
         </div>
       </div>
       <div class="tile is-parent">
-        <article
-          class="tile is-child notification is-primary is-folio-thumbnail"
-        >
-          <p class="title">
-            Wide tile
-          </p>
-          <p class="subtitle">
-            Aligned with the right tile
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum pariatur ullam, mollitia delectus suscipit debitis quis accusamus dignissimos! Deserunt pariatur praesentium totam. Rerum rem quis repellat mollitia molestiae consequatur accusamus?
-          </p>
-          <div class="content">
-          <!-- Content -->
-          </div>
-        </article>
+        <portfolio-article :thumb-image="img1" />
       </div>
     </div>
     <div class="tile is-parent">
@@ -72,68 +59,33 @@
   </div>
 </template>
 <script>
+import PortfolioArticle from '~/components/portfolio/PortfolioArticle.vue'
+
 const firstWealthImg = require('~/assets/images/case_study/first_wealth/the_team.jpg')
 
 export default {
+  components: {
+    PortfolioArticle
+  },
   data () {
     return {
       img1: firstWealthImg
     }
   }
+
 }
 </script>
 
 <style lang="scss" scoped>
-/* Folio Tiles */
-.tile .is-folio-thumbnail {
-  overflow: hidden;
 
-  & .bg-image {
-    opacity: 1;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    transition: all 0.6s;
-    padding: 20px;
-  }
-  &:hover .bg-image {
-    opacity: .4;
-  }
+// article img {
+//   transition: all 0.6s;
+// }
 
-  & .folio-thumbnail-details {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    padding: 20px;
-    border-radius: $radius;
-    margin: 0 important;
-    opacity: 0;
-    transition: all .6s;
-
-    & .title {
-      font-family: $roboto-slab;
-      text-align: center;
-    }
-  }
-
-  &:hover {
-    .folio-thumbnail-details {
-      opacity: 1;
-    }
-  }
-}
-
-article img {
-  transition: all 0.6s;
-}
-
-article:hover img {
-    transform: scale(1.2);
-    cursor: pointer;
-}
+// article:hover img {
+//     transform: scale(1.2);
+//     cursor: pointer;
+// }
 
 // article p {
   // visibility: hidden;
