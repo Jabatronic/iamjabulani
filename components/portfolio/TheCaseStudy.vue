ece7a6332e480755429b65401b0465e4329cc7e1
Jabatronic <jabatronic@gmail.com>
2019-11-20 11:48:54 +0000

    componentize the case studies for modularity

---
+++ components/portfolio/TheCaseStudy.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="container content">
+    <div class="columns is-centered">
+      <div class="column is-9">
+        <h1 class="title is-2 jtron-logotype has-text-centered">
+          Case Study: <slot name="heading" />
+        </h1>
+        <figure class="box">
+          <slot name="header-image">
+            <img src="https://via.placeholder.com/960x474" alt="">
+          </slot>
+        </figure>
+        <h3>Overview</h3>
+
+        <p class="is-size-5">
+          <slot name="overview">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, a quam saepe soluta repellendus blanditiis modi eius ipsum vel. Sit magnam dolorum nesciunt expedita dignissimos, molestias nihil alias cupiditate! Reiciendis?
+          </slot>
+        </p>
+
+        <figure class="box">
+          <slot name="folio-image-1">
+            <img src="https://via.placeholder.com/960x474?text=Folio+Image+1" alt="">
+          </slot>
+        </figure>
+        <h3>What I Did</h3>
+        <div class="columns">
+          <div class="column">
+            <p>
+              <slot name="what-i-did">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro sed vel similique? Commodi eveniet ipsam explicabo neque voluptas, debitis quod accusantium voluptatum rem autem consectetur perferendis labore iste quia modi.
+              </slot>
+            </p>
+          </div>
+          <div class="column">
+            <!-- skills component -->
+          </div>
+        </div>
+
+        <figure class="box">
+          <slot name="folio-image-desktop">
+            <img src="https://via.placeholder.com/960x474?text=Desktop+Mockup" alt="">
+          </slot>
+        </figure>
+        <figure class="box">
+          <slot name="folio-image-mobile">
+            <img src="https://via.placeholder.com/960x474?text=Mobile+Mockup" alt="">
+          </slot>
+        </figure>
+        <div class="folio-image-other">
+          <slot name="folio-image-other">
+            <!-- Any other relevant images -->
+          </slot>
+        </div>
+
+        <h3>Skills Used</h3>
+        <ul>
+          <li>PHP, HTML, JavaScript, CSS, Apache</li>
+          <li>GSAP (JavaScript) Animation</li>
+          <li>Wordpress (including rudimentary plugin development)</li>
+          <li>Photoshop</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CaseStudy',
+  components: {},
+  props: {
+
+  },
+  data () {
+    return {
+      skills: [
+        'PHP, HTML, JavaScript, CSS, Apache',
+        'GSAP (JavaScript) Animation',
+        'Wordpress (including rudimentary plugin development)',
+        'Photoshop'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content figure {
+  margin-left: 0;
+  margin-right: 0;
+}
+
+img {
+  border-radius: 3px;
+}
+</style>
--- pages/case_study_dantanna.vue
+++ pages/case_study_dantanna.vue
@@ -1,77 +1,43 @@
 <template>
-  <div class="container content">
-    <div class="columns">
-      <div class="column is-9">
-        <h1 class="title is-2 jtron-logotype">
-          Case Study: Dantanna Beatz
-        </h1>
-        <h3>Overview</h3>
-        <p>
-          DantannaBeatz is a Birmingham based music producer specialising in full-band live hip-hop. The personell of his projects is fluid with members changing over time and for different projects. Their performances are raucous, frenetic, and unapologetically “DIY” in aesthetic. He required a website that he could update himself, to back up his already active social media presence.
-        </p>
-        <figure class="box">
-          <img :src="require('~/assets/images/case_study/dantanna/dantanna_sections_overview.jpg').src" :srcSet="require('~/assets/images/case_study/dantanna/dantanna_sections_overview.jpg').srcSet">
-        </figure>
-        <figure class="box">
-          <img :src="require('~/assets/images/case_study/dantanna/dantanna_sections_overview.jpg').src" :srcSet="require('~/assets/images/case_study/dantanna/dantanna_sections_overview_2.jpg').srcSet">
-        </figure>
-        <h3>What I Did</h3>
-        <p>I built a onepage, mobile friendly site, backed by a Wordpress CMS. Bright colours with a DIY aesthetic and a lively JavaScript animation in the hero section on loading.</p>
-        <figure class="box">
-          <img :src="require('~/assets/images/case_study/dantanna/dantanna_sections_overview.jpg').src" :srcSet="require('~/assets/images/case_study/dantanna/hero.jpg').srcSet">
-        </figure>
-        <!-- MORE INFO HERE -->
-        <h3>The Focus</h3>
-        <p>I tried to keep the focus on the man himself (Dantanna!). </p>
-        <figure class="box">
-          <img :src="require('~/assets/images/case_study/dantanna/dantanna_sections_overview.jpg').src" :srcSet="require('~/assets/images/case_study/dantanna/first_section.jpg').srcSet">
-        </figure>
-        <!-- MORE INFO HERE -->
-        <h3>Skills Used</h3>
-        <ul>
-          <li>PHP, HTML, JavaScript, CSS, Apache</li>
-          <li>GSAP (JavaScript) Animation</li>
-          <li>Wordpress (including rudimentary plugin development)</li>
-          <li>Photoshop</li>
-        </ul>
-      </div>
-      <div class="column is-3">
-        <!-- Start Sidebar -->
-        <the-sidebar :skills="skills" />
-        <!-- End Sidebar -->
-      </div>
-    </div>
+  <div>
+    <the-case-study>
+      <template slot="heading">
+        DantannaBeatz
+      </template>
+      <template slot="header-image">
+        <!-- Image goes here -->
+      </template>
+      <template slot="overview">
+        <!-- Brief overview of the client, brand, product, etc -->
+      </template>
+      <template slot="folio-image-1">
+        <!-- Brief overview of the client, brand, product, etc -->
+      </template>
+      <template slot="what-i-did">
+        <!-- Outline of how the brief was addressed -->
+      </template>
+      <template slot="folio-image-desktop">
+        <!-- Desktop Mockup -->
+      </template>
+      <template slot="folio-image-mobile">
+        <!-- Mobile mockup -->
+      </template>
+      <template slot="folio-image-other">
+        <!--
+              Any other relevant images
+              possible formats:
+              <picture>, <figure>, <img>, etc
+        -->
+      </template>
+    </the-case-study>
   </div>
 </template>
 
 <script>
-import TheSidebar from '~/components/sidebar/TheSidebar.vue'
-
+import TheCaseStudy from '~/components/portfolio/TheCaseStudy.vue'
 export default {
-  name: 'CaseStudy',
   components: {
-    TheSidebar
-  },
-  data () {
-    return {
-      skills: [
-        'PHP, HTML, JavaScript, CSS, Apache',
-        'GSAP (JavaScript) Animation',
-        'Wordpress (including rudimentary plugin development)',
-        'Photoshop'
-      ]
-    }
+    TheCaseStudy
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.content figure {
-  margin-left: 0;
-  margin-right: 0;
-}
-
-img {
-  border-radius: 3px;
-}
-</style>
