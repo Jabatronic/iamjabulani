db2b53ca2a889b534b7cbb0cf3ee42da3a25456c
Jabatronic <jabatronic@gmail.com>
2019-11-21 16:20:21 +0000

    create new case study pages * containing TheCaseStudy TODO: Make dynamic solution  to make adding new case studies more efficient and scalable

---
+++ pages/case_study_prosperity_health.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <the-case-study />
+    <!-- <the-case-study
+      heading="Testing"
+      headerImage=""
+      overview=""
+      folio-image-1=""
+      solution=""
+      skills=""
+      folioImageDesktop=""
+      folioImageMobile=""
+      folioImageOther=""
+    /> -->
+  </div>
+</template>
+
+<script>
+import TheCaseStudy from '~/components/portfolio/TheCaseStudy.vue'
+export default {
+  components: {
+    TheCaseStudy
+  },
+  computed: {
+    caseData () {
+      return [
+        'PHP, HTML, JavaScript, CSS, Apache',
+        'GSAP (JavaScript) Animation',
+        'Wordpress (including rudimentary plugin development)',
+        'Photoshop'
+      ]
+    }
+  }
+}
+</script>
