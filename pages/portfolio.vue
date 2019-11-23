d14cdda8120c57877b51347a26a0126aa57055ba
Jabatronic <jabatronic@gmail.com>
2019-11-23 23:01:08 +0000

    make header (hero) layout more consistent with other pages

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <h1 class="title is-size-2 has-text-centered is-uppercase has-text-primary has-font-roboto-slab">
-      Selected Works
-    </h1>
-    <h5 class="subtitle has-text-centered">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, minus! Exercitationem, blanditiis. Sint, consectetur tempora!
-    </h5>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-6">
+              <h1 class="title is-size-2 has-text-centered is-uppercase has-text-primary has-font-roboto-slab">
+                Selected Works
+              </h1>
+              <h5 class="subtitle has-text-centered">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, minus! Exercitationem, blanditiis. Sint, consectetur tempora!
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <the-portfolio />
   </div>
 </template>
