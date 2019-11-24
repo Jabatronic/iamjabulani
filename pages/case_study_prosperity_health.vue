a1914f8e51535c20dfb11db46a429b1ca3e9830b
Jabatronic <jabatronic@gmail.com>
2019-11-24 09:44:23 +0000

    make prop order map to the order of display on screen and in markup

--- pages/case_study_prosperity_health.vue
+++ pages/case_study_prosperity_health.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <the-case-study
+      :heading="'Prosperity Health'"
+
       :header-image="require('~/assets/images/case_study/dantanna/dantanna-overview.jpg')"
 
+      :overview="'Healthy body, Healthy mind.'"
+
       :folio-image="require('~/assets/images/case_study/dantanna/dantanna-folio-image.jpg')"
+
+      :solution="`<p>Initially I spent a good deal of time iterating designs in Adobe XD, a process I found very rewarding. During this time my relationship with the company and their products developed and continues to do so.</p>
+      <p>Accomplishing our goals within the budget and timeframe neccessary was a critical factor.</p>
+      <p>I started with a premium Genesis theme and made many modifications and customisations to . `"
 
       :skills="[
         'PHP, HTML, JavaScript, CSS, Apache',
@@ -15,11 +23,6 @@
       :folio-image-desktop="require('~/assets/images/case_study/dantanna/dantanna-folio-image-laptop.png')"
 
       :folio-image-mobile="require('~/assets/images/case_study/dantanna/dantanna-folio-image-mobile.png?size=500')"
-
-      heading="Prosperity Health"
-
-      overview="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto omnis ipsam, laborum iste harum consequatur accusamus vero, quod eaque reprehenderit, illo vel nulla fugit laudantium minus atque soluta eius quas?"
-      solution="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto omnis ipsam, laborum iste harum consequatur accusamus vero, quod eaque reprehenderit, illo vel nulla fugit laudantium minus atque soluta eius quas?"
     />
   </div>
 </template>
