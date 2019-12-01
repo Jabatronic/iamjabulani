5d2ef6f176dbf1e1c8f25b226b54d1524523821b
Jabatronic <jabatronic@gmail.com>
2019-12-01 07:23:49 +0000

    change drop shadow

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -69,6 +69,7 @@ export default {
 }
 
 .jtron-top-nav {
-    box-shadow: 8px 2px 4px rgba(185, 183, 183, 0.2);
+    // box-shadow: 8px 2px 4px rgba(185, 183, 183, 0.2);
+    box-shadow: 0 3px 2px -2px rgba(200,200,200,0.2);
 }
 </style>
