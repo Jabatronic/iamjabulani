5df95ae182c5f60a40f33118a1f516917e8e0acb
Jabatronic <jabatronic@gmail.com>
2019-12-04 23:13:51 +0000

    twaek animations

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
         <div class="container">
           <div class="columns is-centered">
             <div
+              data-aos="fade-down"
+              data-aos-easing="ease-in-out-back"
               class="
                 is-hidden-tablet title is-size-3
                 has-text-centered has-text-teal
@@ -14,7 +16,7 @@
             </div>
             <div class="column is-2-desktop is-2-tablet">
               <figure
-                data-aos="zoom-in"
+                data-aos="zoom-in-right"
                 data-aos-duration="800"
                 data-aos-easing="ease-in-out-back"
                 class="image container is-128x128"
@@ -28,43 +30,18 @@
             </div>
             <div class="column is-6-desktop">
               <div
-                data-aos="fade-up"
+                data-aos="fade-left"
               >
                 <div>
                   <h1 class="title is-size-2 has-text-teal has-font-roboto-slab is-hidden-mobile">
                     <span>I am Jabulani Robbins</span>
                   </h1>
                   <h2 class="title is-size-4 has-text-centered-mobile">
-                    <b>Creative Web Developer, Critical Thinker, Afronaut</b>
+                    <b>Creative Web Developer, Critical Thinker, Afronaut 🚀</b>
                   </h2>
-                  <hr class="is-teal">
-                  <div class="mb-30">
-                    <!--
-                      ToDo: Remove this really crappy way to make a responsive margin!! :/
-                      -->
-                  </div>
+                  <h5 class="subtitle is-size-5 has-text-grey-dark is-italic has-text-centered-mobile">&amp; all round good egg...</h5>
                 </div>
               </div>
-              <!-- <div class="content">
-                <p class="intro-text" data-aos="fade-up">
-                  I made my first website in 2000. Back in the days when table-based layouts, slices, and keyword stuffing was the norm!
-                </p>
-
-                <p class="intro-text" data-aos="fade-up">
-                  Needless to say, things have evolved somewhat since then and I am loving being a Sass(y), Webpack(ing), Javascript(aculous),
-                </p>
-                <p class="intro-text" data-aos="fade-up">
-                  Web Developing Warlock <span class="is-size-3">🧙</span>
-                  ...Or something like that...
-                </p>
-                <p class="intro-text" data-aos="fade-up">
-                  Anyway, you get the picture. So feel the️️️️ ❤️❤️❤️ and check out <nuxt-link to="portfolio">
-                    some of my work
-                  </nuxt-link> OR, if you want to get serious, take a squint at <nuxt-link to="resume">
-                    résumé
-                  </nuxt-link>.
-                </p>
-              </div> -->
             </div>
           </div>
         </div>
