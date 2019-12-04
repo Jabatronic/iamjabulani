f95acc6b6b2a743d096ec343b4fcd36fd05b1305
Jabatronic <jabatronic@gmail.com>
2019-12-04 22:26:33 +0000

    Move typfaces to assets folder

--- modules/typefaces.js
+++ modules/typefaces.js
@@ -1,6 +1,6 @@
 export default function typeface (options) {
   // Add font CSS
-  this.options.css.push('typeface-roboto-slab/index.css')
-  this.options.css.push('typeface-roboto/index.css')
-  this.options.css.push('typeface-raleway/index.css')
+  this.options.css.push('~assets/fonts/typeface-roboto-slab/index.css')
+  this.options.css.push('~assets/fonts/typeface-roboto/index.css')
+  this.options.css.push('~assets/fonts/typeface-raleway/index.css')
 }
