c352331daf2e9d9b990c69addbaffbc9bdfb1191
Jabatronic <jabatronic@gmail.com>
2019-12-06 07:30:50 +0000

    add <p> tags

--- pages/case_study_first_wealth.vue
+++ pages/case_study_first_wealth.vue
@@ -4,7 +4,7 @@
       :heading="'First Wealth'"
       :header-image="require('~/assets/images/case_study/first_wealth/first-wealth-overview.jpg')"
       :overview-title="'A wealth of knowledge in money management...'"
-      :overview="'First Wealth are a personal financial management service. Their branding speaks a clear message of understated sophistication coupled with knowledge and reliability. I was contracted to custom design a section of their website dedicated to their career development programme.'"
+      :overview="'<p>First Wealth are a personal financial management service. Their branding speaks a clear message of understated sophistication coupled with knowledge and reliability. I was contracted to custom design a section of their website dedicated to their career development programme.</p>'"
 
       :folio-image="require('~/assets/images/case_study/first_wealth/first-wealth-folio-image-01.jpg')"
 
@@ -19,7 +19,7 @@
         'Adobe XD, Photoshop'
       ]"
 
-      solution="I built a custom Wordpress template that integrated with their existing (custom built) Wordpress site, and utilised Advanced Custom Fields to implement content management functionality specifically for that area of the site."
+      solution="<p>I built a custom Wordpress template that integrated with their existing (custom built) Wordpress site, and utilised Advanced Custom Fields to implement content management functionality specifically for that area of the site.</p>"
     />
   </div>
 </template>
