a648592ee8e62f732d02402035226573dcbf59cc
Jabatronic <jabatronic@gmail.com>
2019-12-11 09:24:14 +0000

    remove sticky

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,31 +1,20 @@
 <template>
-  <footer class="sticky-footer is-hidden-desktop">
-    <div class="hero-foot">
-      <div class="container">
-        <div class="tabs is-centered">
-          <ul>
-            <li>
-              <a>Contact Me!</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+  <footer class="footer">
+    <div class="has-text-centered">
+      <p>
+        <strong><a href="mailto://jabu.robbins@gmail.com" target="blank">Jabulani Robbins</a></strong>
+        <ul>
+          <li>jabu.robbins@gmail.com</li>
+          <li>+44 7969 910 258</li>
+        </ul>
+      </p>
     </div>
   </footer>
 </template>
 
 <style lang="scss">
-  body {
-    padding-bottom: 80px;
-  }
-  // Footer mods
-  footer.sticky-footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: 10;
-    width: 100vw;
-    background: #fff;
-  }
+.footer {
+  margin-top: $gap;
+  background-color: #fff;
+}
 </style>
