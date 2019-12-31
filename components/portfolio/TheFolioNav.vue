4fa6650d379e3751c02e9db5aab6a9308cbb1eb1
Jabatronic <jabatronic@gmail.com>
2019-12-31 12:27:47 +0000

    create component

---
+++ components/portfolio/TheFolioNav.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button>{{ navLabel }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      navLabel: 'Nav Label'
+    }
+  }
+}
+</script>
