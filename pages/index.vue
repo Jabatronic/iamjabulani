a80a830ca12209a4444afd4b7b5afaf60e76f095
Jabatronic <jabatronic@gmail.com>
2020-02-05 16:59:58 +0000

    minor content mod

--- pages/index.vue
+++ pages/index.vue
@@ -54,10 +54,10 @@
                     data-aos-delay="400"
                     data-aos-easing="ease-in-out-back"
                   >
-                    I use web technologies to make things. I love learning and problem solving and I'm a good communicator who enjoys interacting with people. I'm available for work at the moment so browse some of the projects I've been involved in below (more to come soon!) or, <nuxt-link to="resume">
+                    I use web technologies to make things. I love learning and problem solving and I'm a good communicator who enjoys interacting with people. Please browse some of the projects I've been involved in below (more to come soon!) or, <nuxt-link to="resume">
                       check out my résumé
                     </nuxt-link> and
-                    <a href="mailto:i@iamjabulani.tech" target="blank">get in touch!</a>
+                    <a href="mailto:i@iamjabulani.tech" target="blank">get in touch</a> to find out my availability.
                   </p>
                 </div>
               </div>
